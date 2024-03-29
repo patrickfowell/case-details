ee7d522bada6ec577ee85075f33c58fdb545c737
Patrick Fowell <patrick.fowell@hotmail.co.uk>
2024-03-29 19:22:22 +0000

    [CD-3] proototype of results form for user testing

--- ui-components/src/lib/results-view/results-view.tsx
+++ ui-components/src/lib/results-view/results-view.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import { Case, CaseData } from '../interfaces';
+import { Collapse, Divider, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Paper, Stack, Typography } from '@mui/material';
+import { ExpandLess, StarBorder } from '@mui/icons-material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import PersonIcon from '@mui/icons-material/Person';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 /* eslint-disable-next-line */
 export interface ResultsViewProps {
@@ -8,14 +17,77 @@ export interface ResultsViewProps {
 }
 
 export function ResultsView(props: ResultsViewProps) {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   const results: CaseData = props.data;
+
+  const claimentRows = [
+    {label: "Title", value: results.claimant.title, icon: <PersonIcon />},
+    {label: "First Name", value: results.claimant.firstName, icon: <PersonIcon />},
+    {label: "Last Name", value: results.claimant.lastName, icon: <PersonIcon />},
+    {label: "Date of Birth", value: results.claimant.dateOfBirth, icon: <CalendarMonthIcon />},
+    {label: "Employed From", value:  results.claimant.employedFrom, icon: <CalendarMonthIcon />},
+    {label: "Employed To", value: results.claimant.employedTo, icon: <CalendarMonthIcon />},
+    {label: "Curently Employed", value:  results.claimant.employedCurrently, icon: results.claimant.employedCurrently ? <CheckCircleIcon /> : <CancelIcon/>},
+  ];
+
+  const nonClaimentRows = [
+    {label: "Respondent", value: results.respondent, icon: <PersonIcon />},
+    {label: "Sole Claimant", value: results.soleClaiment, icon: results.soleClaiment ? <CheckCircleIcon /> : <CancelIcon/>},
+    {label: "Date of Receipt", value: results.dateOfReceipt, icon: <CalendarMonthIcon />},
+  ];
+
+  const test: String = "test secondary string";
+
   return (
-    <div>
-      <h2>{`Claimant: ${results.claimant.title} ${results.claimant.firstName} ${results.claimant.lastName}`}</h2>
-      <h2>{`Respondent: ${results.respondent}`}</h2>
-      <h2>{`Sole Claimant: ${results.soleClaiment}`}</h2>
-      <h2>{`Date of Receipt: ${results.dateOfReceipt}`}</h2>
-    </div>
+    <Paper>
+      <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        component="nav"
+        aria-labelledby="results"
+        subheader={
+          <ListSubheader component="div" id="results">
+            Case Details
+          </ListSubheader>
+        }
+      >
+        <ListItemButton onClick={handleClick}>
+          <ListItemIcon>
+            <PersonIcon />
+          </ListItemIcon>
+          <ListItemText primary="Claimant" secondary={`${results.claimant.title} ${results.claimant.firstName} ${results.claimant.lastName}`}/>
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            {claimentRows.map((row) => (
+              <ListItem sx={{ pl: 4 }}>
+              <ListItemIcon>
+                {row.icon}
+              </ListItemIcon>
+              <ListItem>
+                <ListItemText primary={row.label} secondary={row.value.toString()}/>
+              </ListItem>
+            </ListItem>
+            ))}
+          </List>
+        </Collapse>
+        {nonClaimentRows.map((row) => (
+          <ListItem>
+          <ListItemIcon>
+            {row.icon}
+          </ListItemIcon>
+          <ListItem>
+            <ListItemText primary={row.label} secondary={row.value.toString()}/>
+          </ListItem>
+        </ListItem>
+        ))}
+      </List>
+    </Paper>
   );
 }
 
